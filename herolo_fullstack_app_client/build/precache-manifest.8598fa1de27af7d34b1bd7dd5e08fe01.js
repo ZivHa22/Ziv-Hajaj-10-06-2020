75a575b3b4293f1ef0b06154597289fc6b7042fc
Ziv Hajaj <hziv@cambium.co.il>
2020-06-17 12:14:27 +0300

    s

---
+++ herolo_fullstack_app_client/build/precache-manifest.8598fa1de27af7d34b1bd7dd5e08fe01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4302add789f7a074c680c66c563b26f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd11c8a2317008dc565f",
+    "url": "/static/css/main.12e98e49.chunk.css"
+  },
+  {
+    "revision": "0c903b72ff36e2bb736f",
+    "url": "/static/js/2.2969131f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2969131f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd11c8a2317008dc565f",
+    "url": "/static/js/main.96cad624.chunk.js"
+  },
+  {
+    "revision": "f0c492e4e78c11157275",
+    "url": "/static/js/runtime-main.26107c6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
